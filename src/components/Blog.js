794197dade5738027982f53c2df2e9bacfc86ad9
Mike Tran <ngocmike238@gmail.com>
2020-09-09 00:14:31 -0400

    Blog View Update

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,28 +1,44 @@
-import React from 'react'
-import Togglable from './Togglable'
+import React, {useState} from 'react'
 
 const Blog = ({ blog }) => 
 {
+  const [toggle, setToggle] = useState(false)
   const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
+    paddingTop: 5,
+    paddingLeft: 5,
+    paddingBottom: 5,
     border: 'solid',
     borderWidth: 1,
-    marginBottom: 5
+    marginBottom: 5,
+    marginLeft: 2
   }
 
-  const detail = () => (
-    <Togglable buttonLabel='view'>
-      <div style={blogStyle}>
+  const toggleVisible = () => {
+    setToggle(!toggle)
+  }
+
+  const updateLike =() => {
+    // blogService.
+  }
+
+  console.log(toggle)
+  return (
+  <div style={blogStyle}>
+    {
+      toggle === true ? 
+      <>
         {blog.title} by {blog.author} <br></br>
         {blog.url} <br></br>
-        likes {blog.likes} <button>like </button> <br></br>
-      </div>
-    </Togglable>
-  )
-  return (
-  <div>
-    {blog.title} {blog.author} {detail()}
+        likes {blog.likes} <button onClick={updateLike}>like </button> <br></br>
+        <button> Delete! </button>  <button onClick={toggleVisible}> hide </button>
+      </>
+      :
+      <>
+
+      {blog.title} {blog.author} <button onClick={toggleVisible}> view </button>
+      </>
+    }
+
   </div>
   )
 }
